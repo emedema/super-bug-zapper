a4acd39d0087e2ab0488cbe08880e916f3fc41a2
emedema <medema.emily@gmail.com>
2021-10-01 10:44:56 -0700

    black background and white 'plate' has been added as well as basic instructions

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,33 @@
 var vertexShaderText = [
     'precision mediump float;',
     
-    'attribute vec2 vertPosition;',
+    'attribute vec3 vertPosition;',
     
     'void main()',
     '{',
-    '	gl_Position = vec4(vertPosition,0.0,1.0);',
+    '	gl_Position = vec4(vertPosition,1.0);',
     '}'
     ].join('\n');
     
     var fragmentShaderText =
     [
     'precision mediump float;',
-    
+    'uniform vec4 colorAttrib;',
     
     'void main()',
     '{',
         
-    '	gl_FragColor = vec4(1,1,0,1);',
+    '	gl_FragColor = colorAttrib;',
     '}',
     ].join('\n')
+
+	///////////////////////////////////
+	//       Game Variables	         //
+	///////////////////////////////////
+
+	//radius and size for game circle
+	var r = 0.8;
+	var i = 0.5;
 
 var initGame = function(){
 
@@ -28,7 +36,7 @@ var initGame = function(){
 	//////////////////////////////////
 	console.log('this is working');
 
-	var canvas = document.getElementById('game-surface');
+	var canvas = document.getElementById('gameSurface');
 	var gl = canvas.getContext('webgl');
 
 	if (!gl){
@@ -39,8 +47,8 @@ var initGame = function(){
 		alert('your browser does not support webgl');
 	}
 
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	// canvas.width = window.innerWidth;
+	// canvas.height = window.innerHeight;
 	gl.viewport(0,0,canvas.width,canvas.height);
 
 	
@@ -75,5 +83,81 @@ var initGame = function(){
 		return;
 	}
 
+    //////////////////////////////////
+	//    create circle buffer    //
+	//////////////////////////////////
 
+    //all arrays in JS is Float64 by default
+
+	var circleVertexBufferObject = gl.createBuffer();
+	//set the active buffer to the triangle buffer
+	gl.bindBuffer(gl.ARRAY_BUFFER, circleVertexBufferObject);
+	//gl expecting Float32 Array not Float64
+	//gl.STATIC_DRAW means we send the data only once (the triangle vertex position
+	//will not change over time)
+
+	var positionAttribLocation = gl.getAttribLocation(program,'vertPosition');
+	var colorAttrib = gl.getUniformLocation(program, 'colorAttrib')
+	gl.vertexAttribPointer(
+		positionAttribLocation, //attribute location
+		3, //number of elements per attribute
+		gl.FLOAT, 
+		gl.FALSE,
+		0*Float32Array.BYTES_PER_ELEMENT,//size of an individual vertex
+		0*Float32Array.BYTES_PER_ELEMENT//offset from the beginning of a single vertex to this attribute
+		);
+	gl.enableVertexAttribArray(positionAttribLocation);
+
+	gl.useProgram(program);
+
+	//////////////////////////////////
+	//			Functions			//
+	//////////////////////////////////
+
+	function drawCircle(x, y, r, color){
+
+		//init array for storing vertices of circle
+		var circleVertices = [];
+
+		//generate vertices
+		//circle is 360 degrees so 360 vertices
+		//we will be using i so init at 1 and go to <=360
+		for (let i = 1; i<=360; i++){
+			//from x, y find x1, y1 and x2, y2
+			var x1 = r*Math.cos(i)+x;
+			var x2 = r*Math.cos(i+1)+x;
+
+			var y1 = r*Math.sin(i)+y;
+			var y2 = r*Math.sin(i+1)+y;
+
+			circleVertices.push(x);
+			circleVertices.push(y);
+			circleVertices.push(0);
+
+			circleVertices.push(x1);
+			circleVertices.push(y1);
+			circleVertices.push(0);
+
+			circleVertices.push(x2);
+			circleVertices.push(y2);
+			circleVertices.push(0);
+		}
+
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(circleVertices),gl.STATIC_DRAW);
+		gl.uniform4f(colorAttrib, color[0], color[1], color[2], color[3]);
+
+		// Drawing triangles
+		gl.clearColor(0.0,0.0,0.0,1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+		// Draw the triangle 360*3, 3 layers of vertices (disk)
+		gl.drawArrays(gl.TRIANGLES, 0, 360*3);
+
+	}
+	
+	//////////////////////////////////
+	//            Drawing           //
+	//////////////////////////////////
+	
+	drawCircle(0,0,0.8,[1.0, 1.0, 1.0, 1.0]);
+	
 }
